f2343b396aa0457cb13534135359b579d1ad7e46
suzan <59471471+testcreatu@users.noreply.github.com>
2020-09-14 14:44:45 +0545

    Update home-master.blade.php

--- resources/views/home-master.blade.php
+++ resources/views/home-master.blade.php
@@ -116,7 +116,21 @@
 <script type="text/javascript" src="{{url('public/js/js.js')}}"></script>
 <script type="text/javascript" src="{{url('public/js/aos.js')}}"></script>
 <script>
-    AOS.init();
-  </script>
+    AOS.init({
+        disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+        startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+        initClassName: 'aos-init', // class applied after initialization
+        animatedClassName: 'aos-animate', // class applied on animation
+        useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+        disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+        debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+        throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+
+        disable: function() {
+            var maxWidth = 600;
+            return window.innerWidth < maxWidth;
+        }
+    });
+</script>
 
 </html>
